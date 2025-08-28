0e073004ebd209396ea27ad1440ce4317c0c26f8
Hardik <mr.sedhain4740@gmail.com>
2025-08-28 23:19:37 +0545

    milyo hai aaba

--- src/q21.c
+++ src/q21.c
@@ -2,14 +2,15 @@
 #include<stdio.h>
 int main()
 {
-    int x,y,z,temp1,temp2;
+    int x,y,z,temp1,temp2,temp3;
     printf("enter three numbers:");
     scanf("%d %d %d",&x,&y,&z);
      temp1=x;
      temp2=y;
-     x=z;
-     y=temp1;
-     z=temp2;
+     temp3=z;
+     x=temp2;
+     y=temp3;
+     z=temp1;
      printf("After swapping: x=%d y=%d z=%d\n",x,y,z );
      return 0;
 }
