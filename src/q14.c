8183508b9e10d2b26891a9403e755dc3b379a47b
Hardik <mr.sedhain4740@gmail.com>
2025-08-28 22:43:26 +0545

    aaba ta milyo hola

--- src/q14.c
+++ src/q14.c
@@ -5,7 +5,6 @@ int main()
     int a,b;
     printf("enter two numbers:");
     scanf("%d %d",&a,&b);
-     printf("Before swapping: a=%d b=%d",a,b);
      a=a+b;
      b=a-b;
      a=a-b;
--- src/q21.c
+++ src/q21.c
@@ -3,7 +3,6 @@
 int main()
 {
     int x=4,y=6,z=8,temp1,temp2;
-     printf("Before swapping: x=%d y=%d z=%d\n",x,y,z);
      temp1=x;
      temp2=y;
      x=z;
